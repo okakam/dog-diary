c983f04004059de2a96bd3cb1a99683a439d2289
okakam <non@okakam.net>
2025-01-03 20:24:58 +0900

    add: nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View,  StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-gray-900 items-center justify-center">
+      <Text className="text-white">Home screen</Text>
+    </View>
+  );
+}
